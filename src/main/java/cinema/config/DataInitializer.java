6a426297f837a358874fb8e450cd1feadc954530
Roman Polulikh <polilikh.r@gmail.com>
2022-01-19 12:05:19 +0200

    added spring part

---
+++ src/main/java/cinema/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package cinema.config;
+
+import cinema.enums.RoleName;
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.RoleService;
+import cinema.service.UserService;
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public DataInitializer(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setRoleName(RoleName.ADMIN);
+        roleService.add(adminRole);
+        Role userRole = new Role();
+        userRole.setRoleName(RoleName.USER);
+        roleService.add(userRole);
+        User user = new User();
+        user.setEmail("admin@mail.com");
+        user.setPassword("adminPass");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
